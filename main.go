6fccc953bae1502ef24d5b3f61d5767d2a39cb2c
Greg Hellings <greg@thesub.net>
2015-04-13 10:46:10 -0700

    Adding version argument

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import(
 	"log"
+	"fmt"
 	"flag"
 	"path/filepath"
 
@@ -12,13 +13,23 @@ import(
 var configfile string
 var path string
 var branch string
+var version bool
+const (
+	versioninfo = "v0.1.2"
+)
 
 func main(){
 	flag.StringVar(&configfile, "config", "haus.yml", "YAML config file")
 	flag.StringVar(&path, "path", "./hauscfg", "Path to generate files in")
 	flag.StringVar(&branch, "branch", "master", "git branch for hausrepo")
+	flag.BoolVar(&version, "version",false,"haus version")
 	flag.Parse()
-	
+
+	if version == true {
+		fmt.Printf("haus version %s\n", versioninfo)
+		return
+	}
+
 	config,err := haus.ReadConfig(configfile,"~/.hauscfg.yml", branch)
 	if err != nil {
 		log.Fatalf("\n%s",err)
